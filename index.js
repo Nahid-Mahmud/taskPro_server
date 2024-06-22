b9a398f226dca3e3aef888f2f4c92c245fe6bc27
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2024-06-22 12:55:24 +0600

    vercel domain added for cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://taskpro-nahid.surge.sh"],
+    origin: ["http://localhost:5173","https://taskpro-nahid.surge.sh","https://taskpro-nahid.vercel.app"],
   })
 );
 // port
